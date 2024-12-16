847ee422932d24d836ccc89dc12170ef925150cd
js-enjoyer <vasilen.tihin@gmail.com>
2024-12-16 15:32:56 +0200

    Initializing Angular project structure
    Generating all of the components and folders

---
+++ src/app/main/profile/profile-answers/profile-answers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-answers',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-answers.component.html',
+  styleUrl: './profile-answers.component.css'
+})
+export class ProfileAnswersComponent {
+
+}
---
+++ src/app/main/profile/profile-questions/profile-questions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-questions',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-questions.component.html',
+  styleUrl: './profile-questions.component.css'
+})
+export class ProfileQuestionsComponent {
+
+}
